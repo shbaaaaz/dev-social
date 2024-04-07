bd6e8f2acde4f42796b117fbfe6c238f23b23c11
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2024-04-07 17:13:25 +0200

    Feat: Register user, get user auth detail

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const gravatar = require('gravatar')
+const jwt = require('jsonwebtoken')
 const { validationResult, body } = require('express-validator')
+const User = require('../../models/User')
+
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
@@ -15,10 +21,50 @@ router.post(
       .withMessage('Please enter a valid password'),
   ],
 
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
-    if (errors.isEmpty()) return res.send(`Hello ${req.body.name}`)
-    res.status(400).send({ errors: errors.array() })
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() })
+    }
+    const { name, email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] })
+      }
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      })
+
+      const salt = await bcrypt.genSalt(10)
+      user = new User({ name, email, password, avatar })
+      user.password = await bcrypt.hash(password, salt)
+      await user.save()
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err
+          return res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.log(err)
+      res.status(500).send('Server error')
+    }
   }
 )
 
